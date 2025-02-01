683b1852b6621251167efd79e9fdd6a48acef263
Ahmed Ossama <ahmedossama640@gmail.com>
2025-02-01 02:23:36 +0200

    Update readme

--- config/payment.php
+++ config/payment.php
@@ -6,4 +6,15 @@ return [
         'client_secret' => env('PAYPAL_CLIENT_SECRET'),
         'payment_url' => env('PAYPAL_PAYMENT_URL'),
     ]
+    ,
+    'stripe' => [
+        'client_id' => env(strtoupper('STRIPE_CLIENT_ID')),
+        'client_secret' => env(strtoupper('STRIPE_CLIENT_SECRET')),
+        'payment_url' => env(strtoupper('STRIPE_PAYMENT_URL')),
+    ],
+    ,'stripes' => [
+        'client_id' => env(strtoupper('STRIPES_CLIENT_ID')),
+        'client_secret' => env(strtoupper('STRIPES_CLIENT_SECRET')),
+        'payment_url' => env(strtoupper('STRIPES_PAYMENT_URL')),
+    ],
 ];
