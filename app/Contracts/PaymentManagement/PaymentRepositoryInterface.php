2c28f966d954aaa964939edab49dcb905a1df27c
Ahmed Ossama <ahmedossama640@gmail.com>
2025-02-02 11:08:24 +0200

    🔧 Remove unused spaces

--- app/Contracts/PaymentManagement/PaymentRepositoryInterface.php
+++ app/Contracts/PaymentManagement/PaymentRepositoryInterface.php
@@ -8,11 +8,8 @@ use Illuminate\Database\Eloquent\Collection;
 interface PaymentRepositoryInterface
 {
     public function createPayment(array $data): Payment;
-
     public function updatePaymentStatus(int $id, string $paymentId, string $status): bool;
-
     public function getWithFilters(array $filters): Collection;
-
     public function checkPaidOrder($orderId): Collection;
 
 }
